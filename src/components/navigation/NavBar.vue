5355a78d2ef5d1b95bb377ad2294f2a00efacf93
jasper.lievens <jasper.lievens@student.howest.be>
2020-09-07 15:58:22 +0200

    set NavBar as component

---
+++ src/components/navigation/NavBar.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="NavBar">
+        <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-email</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="indigo"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>ToDo Application</v-toolbar-title>
+    </v-app-bar>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  data: () => ({
+    drawer: false
+  })
+}
+</script>
+
+<style>
+
+</style>
