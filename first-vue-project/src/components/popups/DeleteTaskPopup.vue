cdef03840060fb998c555c682ce30fce7759ea07
jasper.lievens <jasper.lievens@student.howest.be>
2020-09-11 11:26:38 +0200

    create a popup when you delete a task

---
+++ first-vue-project/src/components/popups/DeleteTaskPopup.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-card>
+    <v-card-title class="headline">Are you sure you want to delete this task?</v-card-title>
+
+    <v-card-actions>
+      <v-spacer></v-spacer>
+
+      <v-btn
+        color="green darken-1"
+        text
+        @click="dialog = false"
+      >
+        Cancel
+      </v-btn>
+
+      <v-btn
+        color="green darken-1"
+        text
+        @click="dialog = false"
+      >
+        Delete
+      </v-btn>
+    </v-card-actions>
+      </v-card>
+</template>
+
+<script>
+export default {
+  name: 'DeletePopup',
+  data: () => ({
+    dialog: false
+  })
+}
+</script>
+
+<style>
+
+</style>
