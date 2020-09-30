f857074c055ebf49c8bea70b60d2a70977dadc51
jasper.lievens <jasper.lievens@student.howest.be>
2020-09-30 11:47:26 +0200

    add another vue project

--- firebase-app/src/App.vue
+++ firebase-app/src/App.vue
@@ -1,21 +1,28 @@
 <template>
-  <v-app>
-    <MessageSend/>
-  </v-app>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-import MessageSend from './components/Message';
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-
   components: {
-    MessageSend,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+    HelloWorld
+  }
+}
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
